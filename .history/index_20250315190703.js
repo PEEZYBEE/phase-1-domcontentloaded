8a60474eb3c81025411ac4cbb9438f11e0e198f3
keithkipkoech <keithselim@gmail.com>
2025-03-15 19:21:23 +0300

    domcontentloaded

---
+++ .history/index_20250315190703.js
@@ -0,0 +1,8 @@
+// Your code goes heredocument.addEventListener("DOMContentLoaded", () => {
+  // Select the element where you want to change the text
+  const messageElement = document.querySelector("h1"); // Assuming the h1 is the element to be updated
+
+  // Change the content to match the expected result
+  messageElement.textContent = "This is really cool!";
+});
+
